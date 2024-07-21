a9b6a8eded277545488eb0a988900558a33521e5
admin <nazarkorobchuk08@gmail.com>
2024-07-21 22:57:04 +0300

    зробив компонент чату

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import {Routes} from '@angular/router';
 import {LoginPageComponent} from "./pages/login-page/login-page.component";
 import {RegistrationPageComponent} from "./pages/registration-page/registration-page.component";
+import {MessageComponent} from "./message/message.component";
+import {ChatComponentComponent} from "./pages/chat-component/chat-component.component";
 
 export const routes: Routes = [
   {
@@ -10,5 +12,9 @@ export const routes: Routes = [
   {
     path: 'register',
     component: RegistrationPageComponent
+  },
+  {
+    path: 'chat',
+    component: ChatComponentComponent
   }
 ];
