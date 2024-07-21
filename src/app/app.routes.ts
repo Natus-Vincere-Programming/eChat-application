69d2f646c984bb520265dc30c6051c419db64044
admin <ilyakoltsovsky@gmail.com>
2024-07-21 10:21:31 +0300

    Почав робити діалоги

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,10 @@
 import {Routes} from '@angular/router';
+import {MainPageComponent} from "./pages/main-page/main-page.component";
 
 
 export const routes: Routes = [
+  {
+    path: 'main',
+    component: MainPageComponent
+  }
 ];
--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -1,14 +1,37 @@
-import { Component } from '@angular/core';
-import {MatFormField} from "@angular/material/form-field";
+import {ChangeDetectionStrategy, Component, inject, viewChild} from '@angular/core';
+import {MatFormField, MatLabel, MatPrefix, MatSuffix} from "@angular/material/form-field";
+import {MatIcon} from "@angular/material/icon";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatInput} from "@angular/material/input";
+import {MatActionList, MatList, MatListItem} from "@angular/material/list";
+import {NgForOf} from "@angular/common";
+import {MatMenu, MatMenuItem, MatMenuTrigger} from "@angular/material/menu";
+import {AccountDialogComponent} from "./account-dialog/account-dialog.component";
+import {MatDialog} from "@angular/material/dialog";
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
   imports: [
-    MatFormField
+    MatFormField,
+    MatIcon,
+    MatIconButton,
+    MatLabel,
+    MatInput,
+    MatSuffix,
+    MatListItem,
+    MatList,
+    NgForOf,
+    MatMenuTrigger,
+    MatMenu,
+    MatMenuItem,
+    MatPrefix,
+    MatActionList,
+    MatButton
   ],
   templateUrl: './main-page.component.html',
-  styleUrl: './main-page.component.scss'
+  styleUrl: './main-page.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class MainPageComponent {
 
