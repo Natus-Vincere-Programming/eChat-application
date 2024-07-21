438dd5084bc4ae04a7c058bbfc18056b3b91fefa
Ukraine <mnimerovskij@gmail.com>
2024-07-21 21:15:47 +0300

    почав додавати icon

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,11 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {NgClass} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-message',
   standalone: true,
   imports: [
-    NgClass
+    NgClass,
+    MatIcon
   ],
   templateUrl: './message.component.html',
   styleUrl: './message.component.scss'
@@ -13,6 +15,6 @@ import {NgClass} from "@angular/common";
 export class MessageComponent {
   @Input() text: string = '';
   @Input() time: string = '';
-  @Input() stat: string = '';
+  @Input() img_stat!: string;
   @Input() myMess: boolean = true;
 }
