630c774640e136e35740d454bfbd511203a42b2c
Ukraine <mnimerovskij@gmail.com>
2024-07-21 11:27:18 +0300

    почав робити email verification page

---
+++ src/app/pages/email-verification-page/email-verification-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-email-verification-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './email-verification-page.component.html',
+  styleUrl: './email-verification-page.component.scss'
+})
+export class EmailVerificationPageComponent {
+
+}
